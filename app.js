fa37765f0b7738ebfa11bf80a63b427d1a22c06f
anjuinterwork <anju@interwork.biz>
2019-04-28 15:41:53 +0530

    error correcting

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app =express();
-const PORT =process.env.port || 3000;
+const PORT =process.env.PORT || 3000;
 const expressLayouts=require('express-ejs-layouts')
 var index=require('./routes/index');
 var user=require('./routes/user')
